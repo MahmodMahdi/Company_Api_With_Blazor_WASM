a4d48cb78b47508f83cd73ff8e0e23ef5d3b5e60
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-02-08 16:39:42 +0200

    feat: Implement Api Entities

---
+++ WebApi_Project/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WebApi_Demo.Models;
+public class Department
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? DepartmentManager { get; set; }
+   // [JsonIgnore]
+    public virtual List<Employee>? Employees { get; set; }
+}
---
+++ WebApi_Project/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi_Demo.Models;
+public class Employee
+{
+	public int Id { get; set; }
+	public string? Name { get; set; }
+	public decimal Salary { get; set; }
+	public string? Address { get; set; }
+	public int Age { get; set; }
+	public int? departmentId { get; set; }
+	public Department? Department { get; set; }
+}
