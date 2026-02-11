cc3f5a1961040f995fb38ff600df630358247587
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-02-11 13:41:04 +0200

    feat(repository): add contract Repository<T>

---
+++ MyCompany.BlazorUI/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Company.Shared.Responses;
+
+namespace MyCompany.BlazorUI.Repository
+{
+	public interface IRepository <T> where T : class
+	{
+		Task<GeneralResponse<IEnumerable<T>>> GetAllAsync ();
+		Task<GeneralResponse<T>> GetByIdAsync (int id);
+		Task CreateAsync (T entity);
+		Task UpdateAsync (int id ,T entity);
+		Task<GeneralResponse<bool>> DeleteAsync (int id);
+	}
+}
