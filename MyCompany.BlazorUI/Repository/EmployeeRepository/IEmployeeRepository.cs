180beecb12e927e38250ed8aecfe264c1afcdba0
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-02-11 12:48:57 +0200

    feat(Employee): add contract for EmployeeRepository

---
+++ MyCompany.BlazorUI/Repository/EmployeeRepository/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using Company.Shared.Models;
+using Company.Shared.Responses;
+
+namespace MyCompany.BlazorUI.Repository.EmployeeRepository
+{
+	public interface IEmployeeRepository 
+	{
+		Task<GeneralResponse<PagedResult<Employee>>> GetPaged(int pageNumber, int pageSize, string? search);
+
+	}
+}
