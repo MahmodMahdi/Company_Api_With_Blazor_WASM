ff3527b33266b069afd5cac19765cafafdb70bd7
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-02-11 12:20:29 +0200

    feat(account): add contract for Account service

---
+++ WebApi_Project/Service/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using Company_Management_System.Dtos.Account;
+using WebApi_Demo.Authentication;
+using WebApi_Demo.Models;
+
+namespace Company_Management_System.Service
+{
+	public interface IAccountService
+	{
+		Task<GeneralResponse<AuthResponseDto>> RegisterAsync(RegisterUserDto UserDto);
+		Task<GeneralResponse<AuthResponseDto>> LoginAsync(LoginUserDto UserDto);
+	}
+}
