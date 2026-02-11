ad2808500324149d0154de7fbc68853c4eff72cd
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-02-11 12:15:28 +0200

    feat(Employee): Add contract for EmployeeRepository

---
+++ WebApi_Project/interfaces/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Company.Shared.Responses;
+using Company_Management_System.Dtos.Employee;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_Demo.Models;
+
+namespace WebApi_Demo.interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<GeneralResponse<PagedResult<EmployeeDto>>> GetAllAsync(int pageNumber,int pageSize,string? search);
+        Task<GeneralResponse<EmployeeDto>> GetByIdAsync(int id);
+        Task<GeneralResponse<int>> CreateAsync(EmployeeFormDto employeeDto);
+        Task<GeneralResponse<int>> UpdateAsync(int id,EmployeeFormDto employeeDto);
+        Task<GeneralResponse<bool>> DeleteAsync(int id);
+    }
+}
