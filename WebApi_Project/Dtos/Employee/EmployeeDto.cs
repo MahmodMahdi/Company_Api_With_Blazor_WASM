f9d4f068d8807757b51c88181a92e60f76034cc8
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-02-11 12:01:50 +0200

    feat(Employee): Add employee dtos

---
+++ WebApi_Project/Dtos/Employee/EmployeeDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company_Management_System.Dtos.Employee;
+public class EmployeeDto
+{
+	public int Id { get; set; }
+	public string? Name { get; set; }
+	public decimal Salary { get; set; }
+	public string? Address { get; set; }
+	public int Age { get; set; }
+	public int? departmentId { get; set; }
+	public string? departmentName { get; set; }
+}
---
+++ WebApi_Project/Dtos/Employee/EmployeeFormDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company_Management_System.Dtos.Employee
+{
+	public class EmployeeFormDto
+	{
+		[Required]
+		[MinLength(3)]
+		public string? Name { get; set; }
+		[Range(2000, 100000)]
+		[Required]
+		public decimal Salary { get; set; }
+		[Required]
+		public string? Address { get; set; }
+		[Required]
+		[Range(22,50)]
+		public int Age { get; set; }
+		[Required]
+		public int? departmentId { get; set; }
+	}
+}
