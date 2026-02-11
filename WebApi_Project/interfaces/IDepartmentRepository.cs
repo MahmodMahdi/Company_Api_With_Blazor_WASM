c9e6a797f280d968bf9a7d188ce7581aab3d00e6
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-02-11 12:06:58 +0200

    feat(Department): Add contract for DepartmentRepository

---
+++ WebApi_Project/interfaces/IDepartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Company_Management_System.Dtos.Deparment;
+
+namespace WebApi_Demo.interfaces
+{
+	public interface IDepartmentRepository
+	{
+		Task<GeneralResponse<IEnumerable<DepartmentDto>>> GetAllAsync();
+		Task<GeneralResponse<DepartmentWithEmployeesDto>> GetDepartmentWithEmployeesByIdAsync(int id);
+		Task<GeneralResponse<DepartmentDto>> GetByIdAsync(int id);
+		Task<GeneralResponse<int>> CreateAsync(DepartmentFormDto employee);
+		Task<GeneralResponse<int>> UpdateAsync(int id, DepartmentFormDto employee);
+		Task<GeneralResponse<bool>> DeleteAsync(int id);
+	}
+}
