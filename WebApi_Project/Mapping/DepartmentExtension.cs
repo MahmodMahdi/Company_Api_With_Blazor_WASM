451ffdc53b082c54ab6917abc216fc7552e8b369
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-02-11 12:17:49 +0200

    feat(mapping): add extension methods for entity-DTO mapping

---
+++ WebApi_Project/Mapping/DepartmentExtension.cs
@@ -0,0 +1,23 @@
+﻿using Company_Management_System.Dtos.Deparment;
+using WebApi_Demo.Models;
+
+namespace Company_Management_System.Mapping
+{
+	public static class DepartmentExtension
+	{
+		public static DepartmentDto ToDto (this Department model)
+		{
+			return new DepartmentDto
+			{
+				Id = model.Id,
+				Name = model.Name,
+				DepartmentManager = model.DepartmentManager
+			};
+		}
+		public static void ToModel (this DepartmentFormDto dto,Department department)
+		{
+			department.Name = dto.Name;
+			department.DepartmentManager = dto.DepartmentManager;
+		}
+	}
+}
---
+++ WebApi_Project/Mapping/EmployeeExtension.cs
@@ -0,0 +1,31 @@
+﻿using Company_Management_System.Dtos.Employee;
+using WebApi_Demo.Models;
+
+namespace Company_Management_System.Mapping
+{
+	public static class EmployeeExtension
+	{
+		public static EmployeeDto ToDto(this Employee employee)
+		{
+			return new EmployeeDto
+			{
+				Id = employee.Id,
+				Name = employee.Name,
+				Address = employee.Address,
+				Age = employee.Age,
+				Salary = employee.Salary,
+				departmentId = employee.departmentId,
+				departmentName = employee.Department?.Name ?? "No Department"
+
+			};
+		}
+		public static void ToModel(this EmployeeFormDto createEmployeeDto,Employee employee)
+		{
+			employee.Name = createEmployeeDto.Name;
+			employee.Address = createEmployeeDto.Address;
+			employee.Age = createEmployeeDto.Age;
+			employee.Salary = createEmployeeDto.Salary;
+			employee.departmentId = createEmployeeDto.departmentId;
+		}
+	}
+}
