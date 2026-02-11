53b2b79a02452e1fae591704653da826240d2b6f
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-02-11 15:32:58 +0200

    feat(Responses): add GeneralResponse and PagedResult

---
+++ Company.Shared/Responses/GeneralResponse.cs
@@ -0,0 +1,17 @@
+﻿public class GeneralResponse<T>
+{
+	public bool Success { get; set; }
+	public string? Message { get; set; }
+	public T? Data { get; set; } // هنا تضع أي بيانات تريد إرجاعها
+
+	// Constructor للنجاح مع بيانات
+	public GeneralResponse(bool success, string? message, T? data = default)
+	{
+		Success = success;
+		Message = message;
+		Data = data;
+	}
+
+	// Constructor فارغ مهم جداً للـ JSON
+	public GeneralResponse() { }
+}
---
+++ Company.Shared/Responses/PagedResult.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Company.Shared.Responses
+{
+	public class PagedResult<T>
+	{
+		public IEnumerable<T> Items { get; set; }
+		public int PageNumber { get; set; }
+		public int PageSize { get; set; }
+		public int TotalCount { get; set; }
+		public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+		public bool HasPrevious => PageNumber > 1;
+		public bool HasNext => PageNumber < TotalPages; 
+
+		// ✅ أضف Default Constructor للـ JSON Deserialization
+		public PagedResult()
+		{
+			Items = new List<T>();
+		}
+		public PagedResult(IEnumerable<T> items,int count, int pageNumber, int pageSize)
+		{
+			Items = items;
+			TotalCount = count;
+			PageNumber = pageNumber;
+			PageSize = pageSize;
+		}
+		public static async Task<PagedResult<T>> GetPaginated (IQueryable<T> source, int pageNumber, int pageSize)
+		{
+			var count =await source.CountAsync();
+			var items = await source
+				.Skip((pageNumber - 1) * pageSize)
+				.Take(pageSize)
+				.ToListAsync();
+			return new PagedResult<T>(items, count, pageNumber, pageSize);
+		}
+	}
+}
